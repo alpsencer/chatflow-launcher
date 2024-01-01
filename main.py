e030c5bc532d3bec9cac37ae4c098eca061a0b4d
Yavuz Alp Sencer OZTURK <28690608+alpsencer@users.noreply.github.com>
2024-01-01 17:20:57 +0300

    Refactor ChatFlowLauncher class and update query method

--- main.py
+++ main.py
@@ -4,42 +4,41 @@ sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from flowlauncher import FlowLauncher
-import webbrowser
-import urllib.parse
+from flox import Flox
 import json
+import os
+import urllib.parse
+import webbrowser
 from pathlib import Path
 
-class HelloWorld(FlowLauncher):
+class ChatFlowLauncher(Flox):
+    def __init__(self):
+        super().__init__()
+        self.model = self.settings.get("default_model", "gpt-3.5")
 
     def query(self, prompt):
-        base_dir = Path(__file__).resolve().parent
-        Flow_launcher_main_dir = base_dir.parent.parent
-        flow_settings_dir = Flow_launcher_main_dir / "Settings" / "Plugins" / "ChatFlow" / "Settings.json"
-        if os.path.exists(flow_settings_dir):
-            with open(flow_settings_dir,"r") as f:
-                settings=json.load(f)
-        if(settings["default_model"] != "null"):
-            model=settings["default_model"]
-        else:
-            model="text-davinci-002-render-sha"
+        # Choose the model based on the prompt
+        if(self.model == "gpt-3.5"):
+            model = "text-davinci-002-render-sha"
+        model = "gpt-4" if prompt.startswith("4 ") else model
+        prompt = prompt.lstrip("4 ")
         encoded_prompt = urllib.parse.quote(prompt)  # URL encode the prompt
         openai_chat_url = f"https://chat.openai.com/?model={model}&prompt={encoded_prompt}"  # Construct the URL with both parameters
-
-        return [
-            {
-                "Title": f"OpenAI Chat with Model: '{model}' and Prompt: '{prompt}'",
-                "SubTitle": "Press Enter to open in OpenAI Chat",
-                "IcoPath": "Images/app.png",
-                "JsonRPCAction": {
-                    "method": "open_url",
-                    "parameters": [openai_chat_url]  # Pass the constructed URL
-                }
-            }
-        ]
+        if(model == "text-davinci-002-render-sha"):
+            visible_model = "GPT 3.5"
+        elif(model == "gpt-4"):
+            visible_model = "GPT 4"
+        self.add_item(
+            title=f"Chat with: '{visible_model}' and Prompt: '{prompt}'",
+            subtitle="Press Enter to open in OpenAI Chat",
+            icon="Images/app.png",
+            method=self.open_url,
+            parameters=[openai_chat_url]
+        )
+        return
 
     def open_url(self, url):
         webbrowser.open(url)
 
 if __name__ == "__main__":
-    HelloWorld()
+    ChatFlowLauncher()
